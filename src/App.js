5df27729007e6240f7960f2d94592f559a7eef91
Iván Sola Delgado <ivan@Air-de-Ivan.bikoschool>
2023-05-19 17:39:48 +0200

    Bicho en componente parte1

--- src/App.js
+++ src/App.js
@@ -11,9 +11,20 @@ import regla from "./ui/_images/iconos/regla.svg";
   url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
 </style>;
 
+const pokemon = {
+  id: "001",
+  name: "Bulbasaur",
+  height: 4.3,
+  weight: 6.5,
+  types: ["grass", "poison"],
+  src: "https://...",
+  description:
+    "There is a plant seed on its back right from the day this Pokémon is born.",
+};
+
 const Chip = ({ type }) => {
   const styles = {
-    backgroundColor: `var(--color${type})`,
+    backgroundColor: `var(--color-${type})`,
   };
 
   return (
@@ -24,29 +35,34 @@ const Chip = ({ type }) => {
 };
 
 export default function App() {
+  const pokemonWeight = pokemon.weight + " kg";
+  const pokemonHeight = pokemon.height + " m";
+  const pokemonTypes = pokemon.types.map((type) => {
+    return <Chip type={type} />;
+  });
+  const pokemonDescription = pokemon.description;
   return (
     <div className="App">
       <section className="card">
         <div className="card__pokemonIdentity">
-          <h1 className="card__name">Bulbasur</h1>
-          <h2 className="card__index">#001</h2>
+          <h1 className="card__name">{pokemon.name}</h1>
+          <h2 className="card__index">#{pokemon.id}</h2>
         </div>
 
         <div className="card__pokemonDescription">
           <img src={bulbasur} className="card__pictures" />
 
           <article className="card__stats">
-            <div className="card__types">
-              <Chip type="Grass" />
-              <Chip type="Poison" />
-            </div>
+            <div className="card__types">{pokemonTypes}</div>
             <span className="card__about">About</span>
 
             <div className="card__measurments">
               <div className="card__weight">
                 <div className="iconWeight">
                   <img className="card__weightIcon" src={bascula}></img>
-                  <span className="card__weightValue measurments">6.9 kg</span>
+                  <span className="card__weightValue measurments">
+                    {pokemonWeight}
+                  </span>
                 </div>
                 <span className="card__weightTitle measurmentsTitle">
                   Weight
@@ -55,17 +71,16 @@ export default function App() {
               <div className="card__height">
                 <div className="iconHeight">
                   <img className="card__heightIcon" src={regla}></img>
-                  <span className="card__heightValue measurments">0.7 m</span>
+                  <span className="card__heightValue measurments">
+                    {pokemonHeight}
+                  </span>
                 </div>
                 <span className="card__heightTitle measurmentsTitle">
                   Height
                 </span>
               </div>
             </div>
-            <footer className="card__description">
-              There is a plant seed on its back right from the day this Pokémon
-              is born.
-            </footer>
+            <footer className="card__description">{pokemonDescription}</footer>
           </article>
         </div>
       </section>
